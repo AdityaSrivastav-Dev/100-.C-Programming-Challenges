91d761d37b8cff2db4a912b21ef263cd2f5f8c20
AdityaSrivastav-Dev <adityakumar887414@gmail.com>
2026-08-22 09:41:32 +0530

    updated code

---
+++ 15Areaoftriangle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+int a;
+int b;
+
+
+printf("Please enter your Hight");
+scanf("%d",&a);
+printf("Please enter your width");
+scanf("%d",&b);
+
+ 
+ float At ;
+ At =0.5*a*b;
+
+ printf("Area of triangle is ------ %.2f",At);
+
+
+
+ return 0;
+}
