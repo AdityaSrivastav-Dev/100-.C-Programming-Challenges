cb05bfea204868cb254ce2c1eedebf1c9459f5cf
AdityaSrivastav-Dev <adityakumar887414@gmail.com>
2026-08-22 14:19:17 +0530

    updated code21 checking year Leap or not

--- 22leapyear.c
+++ 22leapyear.c
@@ -8,18 +8,33 @@ int main()
   printf("Welcome  to leap Year chacking calander\n");
   printf("please entr your year\n");
 
-  scanf("%d",&year);
-
-  if(year % 400 ==0){
-    printf("%d is a leap year " ,year);
-  }else if (year %100==0){
-    printf("%d is a not leap year " ,year);
-  }else if(year%4==0){
-    printf("%d is a  leap year " ,year);
-  }else{
-    printf("%d is a not leap year " ,year);
-
+  scanf("%d", &year);
+  /// logic first
+  if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+  {
+    printf("your are a leap year ");
   }
+  else
+  {
+    printf("your are a not leap year ");
+  }
+  ///////logic second
+  // if (year % 400 == 0)
+  // {
+  //   printf("%d is a leap year ", year);
+  // }
+  // else if (year % 100 == 0)
+  // {
+  //   printf("%d is a not leap year ", year);
+  // }
+  // else if (year % 4 == 0)
+  // {
+  //   printf("%d is a  leap year ", year);
+  // }
+  // else
+  // {
+  //   printf("%d is a not leap year ", year);
+  // }
 
   return 0;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,2 +1,16 @@
-float hight;
-// float width;
+if (year % 400 == 0)
+    // {
+    //   printf("%d is a leap year ", year);
+    // }
+    // else if (year % 100 == 0)
+    // {
+    //   printf("%d is a not leap year ", year);
+    // }
+    // else if (year % 4 == 0)
+    // {
+    //   printf("%d is a  leap year ", year);
+    // }
+    // else
+    // {
+    //   printf("%d is a not leap year ", year);
+    // }
