90d7233a2718fe3b34160d1d5cbe61d6f410ee7f
AdityaSrivastav-Dev <adityakumar887414@gmail.com>
2026-08-25 22:33:59 +0530

    using ternory absolute number d

---
+++ 27absolute.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  int n;
+
+  printf("Please Your number ");
+  scanf("%d", &n);
+printf("%d is absole value ",n > 0 ? n:-n);
+  return 0;
+}
