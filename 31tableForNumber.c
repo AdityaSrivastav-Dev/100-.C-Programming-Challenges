d8661096d089f69c790cf5ff324164eb9613393d
AdityaSrivastav-Dev <adityakumar887414@gmail.com>
2026-08-26 09:29:36 +0530

    table for any number which given by user

---
+++ 31tableForNumber.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+  int num1  ;
+ 
+
+  printf("------ Welcome Table calculater for any number ------ \n");
+  printf("Please Your first number  \n");
+  scanf("%d", &num1);
+printf(" \n number x 1 :- %d",num1*1);
+printf(" \n number x 2 :- %d",num1*2);
+printf(" \n number x 3 :- %d",num1*3);
+printf(" \n number x 4 :- %d",num1*4);
+printf(" \n number x 5 :- %d",num1*5);
+printf(" \n number x 6 :- %d",num1*6);
+printf(" \n number x 7 :- %d",num1*7);
+printf(" \n number x 8 :- %d",num1*8);
+printf(" \n number x 9 :- %d",num1*9);
+printf(" \n number x 10 :- %d",num1*10);
+
+  return 0;
+}
